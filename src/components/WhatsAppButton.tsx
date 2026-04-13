d9278aa636f9be9d1b90c7a6262aa1df3e2d25a3
Ahmad Ali <khawajaali1020@gmail.com>
2026-04-14 02:22:07 +0500

    Update WhatsAppButton.tsx

--- src/components/WhatsAppButton.tsx
+++ src/components/WhatsAppButton.tsx
@@ -5,7 +5,7 @@ import { FaWhatsapp } from "react-icons/fa";
 export default function WhatsAppButton() {
   return (
     <a
-      href="https://wa.me/13072055481"
+      href="https://wa.me/13073938789"
       target="_blank"
       rel="noopener noreferrer"
       className="whatsapp-float"
