863623fc19a22d79e552f0e1977a6f32aa177409
Ahmad Ali <khawajaali1020@gmail.com>
2026-04-14 02:29:48 +0500

    Update SuccessStoriesPreview.tsx

--- src/components/home/SuccessStoriesPreview.tsx
+++ src/components/home/SuccessStoriesPreview.tsx
@@ -69,7 +69,7 @@ export default function SuccessStoriesPreview() {
             More Success Stories
           </Link>
           <a
-            href="https://wa.link/m2ac6m"
+            href="https://wa.me/message/Q6XJRMT4HKTBF1"
             target="_blank"
             rel="noopener noreferrer"
             className="btn-outline"
